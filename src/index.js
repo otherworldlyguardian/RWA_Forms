bfc69bc16cc8e13aca5b464f900be801405977d6
otherworldlyguardian <otherworldlyguardian@gmail.com>
2018-06-15 13:13:10 -0400

    initial redux setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import 'semantic-ui-css/semantic.min.css'
+import './index.css'
+import allReducers from './reducers'
+import App from './App'
+
+const store = createStore(allReducers)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
